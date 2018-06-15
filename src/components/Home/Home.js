c937ce9254d6bcdf5ffd10de9ace4559892f59f3
Cesar Mejia Leiva <cmejia@gmail.com>
2018-06-15 14:01:01 -0700

    home wip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,37 @@ import carouselMovie from '../../assets/carousel_movie.mp4'
 import carouselMars from '../../assets/carousel_mars.jpg'
 import './Home.css'
 
-const Home = () => <div />
+// <video className='carousel__movie' playsinline muted loop>
+// <source src={carouselMovie} type='video/mp4' />
+// </video>
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='wrapper__movie'>
+        <img className='carousel__image' src={carouselRocket} />
+        <div className='carousel__overlay'>
+          <h2 className='carousel__title--active'>Falcon Heavy & Starman</h2>
+          <button className='carousel__button--active'>Watch Video</button>
+        </div>
+      </div>
+
+      <div className='carousel__menu'>
+        <div className='carousel__menu-item'>
+          <div className='carousel__menu-num'>01</div>
+          <div className='carousel__menu-title'>SES-12 MISSION</div>
+        </div>
+        <div className='carousel__menu-item'>
+          <div className='carousel__menu-num'>02</div>
+          <div className='carousel__menu-title'>FALCON HEAVY & STARMAN</div>
+        </div>
+        <div className='carousel__menu-item'>
+          <div className='carousel__menu-num'>03</div>
+          <div className='carousel__menu-title'>MAKING LIFE MULTIPLANETARY</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Home
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,19 +7,37 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      <img className='logo' src={logo} alt='spacex logo' />
+      <Link to='/'>
+        <img className='logo' src={logo} alt='spacex logo' />
+      </Link>
       <div className='rockets'>
-        <div className='falcon9'>FALCON 9</div>
-        <div className='falconH'>FALCON HEAVY</div>
-        <div className='dragon'>DRAGON</div>
+        <Link className='rocket falcon9' to='/falcon9'>
+          FALCON 9
+        </Link>
+        <Link className='rocket falcon-heavy' to='falcon-heavy'>
+          FALCON HEAVY
+        </Link>
+        <Link className='rocket dragon' to='dragon'>
+          DRAGON
+        </Link>
       </div>
-      <div className='news'>NEWS</div>
-      <div className='company'>
-        <div className='about'>ABOUT SPACEX</div>
-        <div className='careers'>CAREERS</div>
-        <div className='gallery'>GALLERY</div>
+      <Link className='news' to='/news'>
+        NEWS
+      </Link>
+      <div className='company-info'>
+        <Link className='company about' to='/about'>
+          ABOUT SPACEX
+        </Link>
+        <Link className='company careers' to='/careers'>
+          CAREERS
+        </Link>
+        <Link className='company gallery' to='/gallery'>
+          GALLERY
+        </Link>
       </div>
-      <div className='shop'>SHOP</div>
+      <Link className='shop' to='/shop'>
+        SHOP
+      </Link>
     </div>
   )
 }
